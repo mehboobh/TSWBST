a432bed7942439bc9c7ef511e6c0ddfdcb25e0e9
mehboobh <mehboobhaq15@gmail.com>
2026-08-16 08:13:21 -0400

    Change import path for CtaBand component

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import {
 import { Testimonials } from '@/components/home/testimonials'
 
 import { RiskCta } from '@/components/home/risk-cta'
-import { CtaBand } from '@/components/home/cta-band'
+import { CtaBand } from '@/components/ui/cta-band'
 
 export default function HomePage() {
   return (
